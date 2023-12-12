7b766998e41903a186e8af0a98a5239f1b8183e9
alialahmad68 <ali.alahmad.cs@gmail.com>
2023-12-12 04:31:25 +0200

    socket.io

--- Coinify/src/Context/UserContext.jsx
+++ Coinify/src/Context/UserContext.jsx
@@ -1,24 +1,41 @@
 import React, { createContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
+import { io } from "socket.io-client";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const token = localStorage.getItem("token");
+  const [socket, setSocket] = useState(null);
+
+  const decodeToken = (token) => {
+    try {
+      return jwtDecode(token);
+    } catch (error) {
+      console.error("Error decoding token:", error);
+      return null;
+    }
+  };
 
   useEffect(() => {
-    const getUser = () => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        const decodedToken = jwtDecode(token);
-        setUser(decodedToken);
-      }
-    };
-    getUser();
+    setSocket(io("http://localhost:5000"));
   }, []);
 
+  useEffect(() => {
+    const decodedUser = decodeToken(token);
+    setUser(decodedUser);
+  }, [token]);
+
+  useEffect(() => {
+    if (socket && user) {
+      socket.emit("newUser", user.email);
+      console.log("Context", socket);
+    }
+  }, [socket, user]);
+
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, socket, setSocket }}>
       {children}
     </UserContext.Provider>
   );
